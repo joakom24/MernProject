75391b33db264c06c3a5d46f8fa655a7d9d1b512
joakom24 <joakoomussi@gmail.com>
2022-01-25 20:56:27 -0300

    DB CONNECTED

--- server/index.js
+++ server/index.js
@@ -20,5 +20,5 @@ app.get("/", (req, res) => {
 //PORT
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
-  console.log("Server running on port: ", port);
+  console.log("Server running on PORT: ", port);
 });
