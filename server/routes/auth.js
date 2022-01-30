510e8fd8eff878879a7f0a105e21eca90e0af95c
joakom24 <joakoomussi@gmail.com>
2022-01-30 18:42:53 -0300

    Bcrypt

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,30 +2,37 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
-
+const bcrypt = require("bcryptjs");
 router.post("/signup", (req, res) => {
   var { name, email, password } = req.body;
   console.log(req.body);
   if (!email || !password || !name) {
-    return res.status(422).json({ error: "Error" });
+    return res.status(200).json({ error: "Add all data" });
   }
-  User.findOne({ email: email })
-    .then((savedUser) => {
-      if (savedUser) {
-        return res
-          .status(422)
-          .json({ error: "User already exists with that email" });
-      }
-      const user = new User({
-        email,
-        password,
-        name,
-      });
-      user
-        .save()
-        .then((user) => {
-          res.json({ message: "Saved Successfully" });
-          console.log(user.email);
+  bcrypt
+    .hash(password, 12)
+    .then((hashedpw) => {
+      User.findOne({ email: email })
+        .then((savedUser) => {
+          if (savedUser) {
+            return res
+              .status(422)
+              .json({ error: "User already exists with that email" });
+          }
+          const user = new User({
+            email,
+            password: hashedpw,
+            name,
+          });
+          user
+            .save()
+            .then((user) => {
+              res.json({ message: "Saved Successfully" });
+              console.log(user.email);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
         })
         .catch((err) => {
           console.log(err);
