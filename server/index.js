7e9d93bde42d91990a2effd8a88667b4400ca346
joakom24 <joakoomussi@gmail.com>
2022-01-30 18:35:28 -0300

    Register success

--- server/index.js
+++ server/index.js
@@ -1,21 +1,16 @@
 const express = require("express");
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
-const expressValidator = require("express-validator");
 //
 const app = express();
+
 require("dotenv").config();
+require("./models/userSchema");
 
 //Middlewares
-
+app.use(express.json());
+app.use(require("./routes/auth"));
 //Connect db
 const connectDB = require("./config/db");
 connectDB();
-
-//Routes
-app.get("/", (req, res) => {
-  res.send("Hola mundo");
-});
 
 //PORT
 const port = process.env.PORT || 4000;
---
+++ server/routes/auth.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+
+router.post("/signup", (req, res) => {
+  var { name, email, password } = req.body;
+  console.log(req.body);
+  if (!email || !password || !name) {
+    return res.status(422).json({ error: "Error" });
+  }
+  User.findOne({ email: email })
+    .then((savedUser) => {
+      if (savedUser) {
+        return res
+          .status(422)
+          .json({ error: "User already exists with that email" });
+      }
+      const user = new User({
+        email,
+        password,
+        name,
+      });
+      user
+        .save()
+        .then((user) => {
+          res.json({ message: "Saved Successfully" });
+          console.log(user.email);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+module.exports = router;
